e904ac3e291d3f77e0b17aa6b5d69c9b65508127
Florian Lang <florian.lang@students.fhnw.ch>
2020-04-07 18:21:02 +0200

    created SubmitActivity

--- app/src/main/java/com/example/ecoquiz/AboutActivity.java
+++ app/src/main/java/com/example/ecoquiz/AboutActivity.java
@@ -12,12 +12,12 @@ public class AboutActivity extends AppCompatActivity implements AboutFragment.Br
 
         AboutFragment aboutFrag = new AboutFragment();
         aboutFrag.setBroListener(this);
-        getSupportFragmentManager().beginTransaction().replace(R.id.aboutFragmentContainer, aboutFrag).addToBackStack(null).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.aboutFragmentContainer, aboutFrag).commit();
     }
 
     @Override
     public void onLongClick() {
         AboutSecret secretFrag = new AboutSecret();
-    getSupportFragmentManager().beginTransaction().replace(R.id.aboutFragmentContainer, secretFrag).commit();
+    getSupportFragmentManager().beginTransaction().replace(R.id.aboutFragmentContainer, secretFrag).addToBackStack(null).commit();
     }
 }
