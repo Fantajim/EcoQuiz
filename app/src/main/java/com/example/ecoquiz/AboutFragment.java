8b9582280fe5fb9fdd91d36a2c2ccf331bdcb250
Florian Lang <florian.lang@students.fhnw.ch>
2020-04-07 14:50:33 +0200

    Imageview clickable

--- app/src/main/java/com/example/ecoquiz/AboutFragment.java
+++ app/src/main/java/com/example/ecoquiz/AboutFragment.java
@@ -1,10 +1,13 @@
 package com.example.ecoquiz;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -14,6 +17,7 @@ import android.widget.TextView;
 public class AboutFragment extends Fragment {
     private BroListener listener;
     private TextView broButton;
+    private ImageView ivGitlab;
 
     public void setBroListener(BroListener mListener) {
         listener = mListener;
@@ -31,6 +35,7 @@ public class AboutFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
         broButton = view.findViewById(R.id.tvBro);
+        ivGitlab = view.findViewById(R.id.ivGitlab);
 
         broButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -40,8 +45,15 @@ public class AboutFragment extends Fragment {
             }
         });
 
-
-
+        ivGitlab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://gitlab.fhnw.ch/florian.lang/ecoquiz"));
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
 
         return view;
     }
